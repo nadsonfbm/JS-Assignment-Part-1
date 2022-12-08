9d295c116aad4ba0c3480549038dc866b39f70c1
Nadson Mendes <nadsonfbm@hotmail.com>
2022-12-08 18:22:25 -0300

    3rd commit

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let playersWins = 0;
-let computerWins = 0;
+let computersWins = 0;
 
 function computerPlay() {
     let computerChoise = ["Rock", "Paper", "Scissor"];
@@ -9,19 +9,19 @@ function computerPlay() {
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === "ROCK" && computerSelection === "Paper") {
         console.log("Oh no! Paper covers Rock! You lost!");
-        computerWins = computerWins + 1;
+        computersWins = computersWins + 1;
     } else if (playerSelection === "ROCK" && computerSelection === "Scissor") {
         console.log ("Great choice! Rock crushes Scissor! You won!");
         playersWins = playersWins + 1;
     } else if (playerSelection === "SCISSOR" && computerSelection === "Rock") {
         console.log ("Oh no! Rock crushes Scissor! You lost!");
-        computerWins = computerWins + 1;
+        computersWins = computersWins + 1;
     } else if (playerSelection === "SCISSOR" && computerSelection === "Paper") {
         console.log ("Great choice! Scissor cuts Paper! You won!");
         playersWins = playersWins + 1;
     } else if (playerSelection === "PAPER" && computerSelection === "Scissor") {
         console.log ("Oh no! Scissor cuts Paper! You lost!");
-        computerWins = computerWins + 1;
+        computersWins = computersWins + 1;
     } else if (playerSelection === "PAPER" && computerSelection === "Rock") {
         console.log ("Great choice! Paper covers Rock! You won!");
         playersWins = playersWins + 1;
@@ -39,11 +39,11 @@ function game() {
         let playerAnswer = answer.toLowerCase().trim();
         if (playerAnswer === "rock" || playerAnswer === "paper" || playerAnswer === "scissor") {
             playRound(playerSelection, computerSelection);
-            if (playerWins === 3) {
+            if (playersWins === 3) {
                 console.log("You won 3 matches first!! Congratulations!! Game Over.");
                 break;
             }
-            if (computerWins === 3) {
+            if (computersWins === 3) {
                 console.log("You lost 3 matches!! That's what i call a bad luck! Game Over.");
                 break;
             } else {
